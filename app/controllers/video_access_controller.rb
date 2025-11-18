6b5ee3ddbd9143b1c9156782c17d6facba30bba8
Huteikei11 <andoromedazadazo11@gmail.com>
2025-11-19 00:29:50 +0900

    Require login for all video access
    - Remove exception for :player action in VideosController
    - Remove guest user temporary access via session
    - Require authentication for all VideoAccessController actions
    - Users must be logged in to view any videos

--- app/controllers/video_access_controller.rb
+++ app/controllers/video_access_controller.rb
@@ -1,16 +1,10 @@
 class VideoAccessController < ApplicationController
-  before_action :authenticate_user!, except: [ :index, :new, :create ]
+  before_action :authenticate_user!
 
   def index
-    if user_signed_in?
-      @public_videos = Video.public_videos
-      @accessible_videos = current_user.accessible_videos.private_videos
-      @my_videos = current_user.created_videos
-    else
-      @public_videos = Video.public_videos
-      @accessible_videos = []
-      @my_videos = []
-    end
+    @public_videos = Video.public_videos
+    @accessible_videos = current_user.accessible_videos.private_videos
+    @my_videos = current_user.created_videos
   end
 
   def new
@@ -26,18 +20,11 @@ class VideoAccessController < ApplicationController
     end
 
     if video.authenticate_with_password(params[:password])
-      if user_signed_in?
-        # ログイン済みユーザーはアクセス権限を追加
-        unless current_user.can_access_video?(video)
-          current_user.video_accesses.create(video: video)
-        end
-        redirect_to player_video_path(video), notice: "動画へのアクセスが許可されました。"
-      else
-        # 未ログインユーザーは一時的にセッションで管理
-        session[:temp_video_access] ||= []
-        session[:temp_video_access] << video.id unless session[:temp_video_access].include?(video.id)
-        redirect_to player_video_path(video), notice: "動画へのアクセスが許可されました。"
+      # ログイン済みユーザーはアクセス権限を追加
+      unless current_user.can_access_video?(video)
+        current_user.video_accesses.create(video: video)
       end
+      redirect_to player_video_path(video), notice: "動画へのアクセスが許可されました。"
     else
       redirect_to video_access_new_path, alert: "パスワードが正しくありません。"
     end
